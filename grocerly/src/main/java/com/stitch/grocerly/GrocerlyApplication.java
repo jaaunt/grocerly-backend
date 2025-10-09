9012987947bb8dd0c441ae1cfed24ae656fb3bcf
jaaunt <jaaunt@taltech.ee>
2025-10-09 16:17:55 +0300

    Spring boot + gradle setup

---
+++ grocerly/src/main/java/com/stitch/grocerly/GrocerlyApplication.java
@@ -0,0 +1,13 @@
+package com.stitch.grocerly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocerlyApplication.class, args);
+	}
+
+}
---
+++ grocerly/src/test/java/com/stitch/grocerly/GrocerlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stitch.grocerly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
