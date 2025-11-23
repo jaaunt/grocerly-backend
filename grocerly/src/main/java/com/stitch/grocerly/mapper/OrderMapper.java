ed8fad93ef7a7df3be98feecc1a294777c49bc44
jaaunt <jaaunt@taltech.ee>
2025-11-23 16:53:35 +0200

    mapper reporting policy

--- grocerly/src/main/java/com/stitch/grocerly/mapper/OrderMapper.java
+++ grocerly/src/main/java/com/stitch/grocerly/mapper/OrderMapper.java
@@ -3,8 +3,9 @@ package com.stitch.grocerly.mapper;
 import com.stitch.grocerly.controller.OrderDto;
 import com.stitch.grocerly.repository.OrderEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface OrderMapper {
 
     OrderEntity toEntity(OrderDto dto);
